f4abe3f7066af8fcd363e9aa72008cc90838d5ad
baranki-gnu <98429714+baranki-gnu@users.noreply.github.com>
2022-05-22 11:42:31 +0300

    частично оптимизирует графику и подгоняет под pixel perfect

---
+++ source/script.js
@@ -0,0 +1,14 @@
+let mainNav = document.querySelector('.main-nav');
+let navToggle = document.querySelector('main-nav__toggle-button');
+
+mainNav.classList.remove('main-nav--nojs');
+
+navToggle.addEventListener('click', function () {
+    if (mainNav.classList.contains('main-nav--closed')) {
+        mainNav.classList.remove('main-nav--closed');
+        mainNav.classList.add('main-nav--opened');
+    } else {
+        mainNav.classList.remove('main-nav--opened');
+        mainNav.classList.add('main-nav--closed');        
+    }
+});
